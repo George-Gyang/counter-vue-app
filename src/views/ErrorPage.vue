a32a348974d26e46af994e5213a74d33f05c0975
George-Gyang <georgegyang3@gmail>
2023-03-22 07:25:15 +0100

    modification

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        <h1>
+    <div class="error-pg">
+        <h2>
             OH NO!!! PAGE NOT FOUND
-        </h1>
+        </h2>
         <p>please enter a valid URL or <span><RouterLink to="/"><i>RETURN</i></RouterLink> </span></p>
     </div>
 </template>
+
+<style scoped>
+.error-pg{
+    margin-left: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,27 @@
 <template>
-    <h2>Conter App </h2>
-    <p>This is a counter project that increment, decrement and reset counter </p>
-    <RouterLink to="/Counter">See more...</RouterLink>
+    <div class="home-content">
+        <h1>Conter App</h1>
+  <p>
+    This is a counter project that increment, decrement set value and reset
+    counter, built using the composition API. The the ref method was use to
+    manage the state and the watch menthod was use to monitor changes from the
+    input element.
+  </p>
+  <RouterLink to="/Counter">See more...</RouterLink>
+    </div>
+
 </template>
+
+
+<style scoped>
+h1{
+    margin: 1rem 0;
+    text-align: center;
+}
+.home-content{
+    max-width: 350px;
+    margin: auto;
+    /* text-align: center; */
+}
+
+</style>
